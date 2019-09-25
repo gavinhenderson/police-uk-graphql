586f4d03f9f2fe11794cd604d54659aabd4ff7c1
Gavin Henderson <gavin.henderson@hotmail.co.uk>
2019-09-25 17:08:11 +0100

    Now works with graphiql

--- main.go
+++ main.go
@@ -7,6 +7,33 @@ import (
 	"github.com/graphql-go/handler"
 )
 
+type user struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var data map[string]user
+
+/*
+   Create User object type with fields "id" and "name" by using GraphQLObjectTypeConfig:
+       - Name: name of object type
+       - Fields: a map of fields by using GraphQLFields
+   Setup type of field use GraphQLFieldConfig
+*/
+var userType = graphql.NewObject(
+	graphql.ObjectConfig{
+		Name: "User",
+		Fields: graphql.Fields{
+			"id": &graphql.Field{
+				Type: graphql.String,
+			},
+			"name": &graphql.Field{
+				Type: graphql.String,
+			},
+		},
+	},
+)
+
 var queryType = graphql.NewObject(
 	graphql.ObjectConfig{
 		Name: "Query",
